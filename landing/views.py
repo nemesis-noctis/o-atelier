20b505dfa6cf6b289675f774cf86d1aa75c2225d
Nemesis Noctis <nemesis.noctisx@gmail.com>
2026-05-26 15:32:36 -0300

    feat: Add login and register pages and adjust templates.

--- landing/views.py
+++ landing/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "landing/index.html")
+
+
+def login(request):
+    return render(request, "landing/login.html")
+
+
+def register(request):
+    return render(request, "landing/register.html")
