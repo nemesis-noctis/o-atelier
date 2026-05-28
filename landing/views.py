1089863dd496ed403c7727f6f22bf6294a2a7d0f
Nemesis Noctis <nemesis.noctisx@gmail.com>
2026-05-28 19:18:27 -0300

    feat: Update landing page to get data from database.

--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
+from core.utils import get_landing_data
+
 
 # Create your views here.
 def index(request):
-    return render(request, "landing/index.html")
+    return render(request, "landing/index.html", context={"data": get_landing_data()})
