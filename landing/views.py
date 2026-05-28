09c37c337225efc513534dd3265ef83f2799603e
Nemesis Noctis <nemesis.noctisx@gmail.com>
2026-05-28 20:14:38 -0300

    feat: Get gallery images and tags from database and adjust layout.

--- landing/views.py
+++ landing/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 from core.utils import get_landing_data
+from .models import GalleryTag, GalleryImage
 
 
 # Create your views here.
 def index(request):
-    return render(request, "landing/index.html", context={"data": get_landing_data()})
+    gallery_tags = GalleryTag.objects.all()
+    gallery_images = GalleryImage.objects.all()
+    context = {
+        "data": get_landing_data(),
+        "gallery_tags": gallery_tags,
+        "gallery_images": gallery_images
+    }
+    return render(request, "landing/index.html", context=context)
