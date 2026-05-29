fca3a95ab0036b127f60219e5f322ebbc9f3ce3b
Nemesis Noctis <nemesis.noctisx@gmail.com>
2026-05-28 23:38:50 -0300

    feat: Add functional tag filtering in gallery using htmx

--- landing/views.py
+++ landing/views.py
@@ -14,3 +14,16 @@ def index(request):
         "gallery_images": gallery_images
     }
     return render(request, "landing/index.html", context=context)
+
+
+def gallery_image_filter(request):
+    tag_name = request.GET.get("tag", "")
+    if tag_name == "all" or tag_name == "":
+        gallery_images = GalleryImage.objects.all()
+        return render(request, template_name="landing/partials/gallery_images.html",
+                      context={"gallery_images": gallery_images})
+
+    else:
+        gallery_images = GalleryImage.objects.filter(tag=GalleryTag.objects.get(tag=tag_name))
+        return render(request, template_name="landing/partials/gallery_images.html",
+                      context={"gallery_images": gallery_images})
