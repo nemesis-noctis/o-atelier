d408078a692cbd1ff52b9531a7d88ab6f752bb70
Nemesis Noctis <nemesis.noctisx@gmail.com>
2026-06-14 23:19:07 -0300

    feat: Add notification model and make migrations.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from accounts.models import CustomUser
+
+
 # Create your models here.
+class Notification(models.Model):
+    template_key = models.CharField(max_length=60)
+    context = models.JSONField(default=dict)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
