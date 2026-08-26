b76011e60636500e1e5c87cb4bd2849ca927ef8c
nemesis_noctis <nemesis.noctisx@gmail.com>
2026-08-26 16:24:58 -0300

    build: Change django database to postgresql and add database service to docker compose with mock data.

--- o_atelier/settings.py
+++ o_atelier/settings.py
@@ -94,9 +94,13 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "database"),
+        "USER": os.getenv("POSTGRES_USER", "admin"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "admin"),
+        "HOST": os.getenv("POSTGRES_HOST", "database"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
